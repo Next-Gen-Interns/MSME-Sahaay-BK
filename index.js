c60e05b92f8a211b59160d100cafdc7f1227e8e1
DhruvJansari <dhruvjansari281@gmail.com>
2026-02-22 16:16:57 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const prisma = new PrismaClient();
+const Ip = process.env.IP;
 
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
@@ -31,6 +32,7 @@ const io = new SocketIOServer(server, {
       "https://msmeguru.crmnextgen.in",
       "http://localhost:3000",
       "http://localhost:3001",
+      Ip,
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
@@ -46,6 +48,7 @@ app.use(
       "https://msmeguru.crmnextgen.in",
       "http://localhost:3000",
       "http://localhost:3001",
+      Ip,
     ],
     credentials: true,
   }),
